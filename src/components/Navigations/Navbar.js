7a6b3c87073c22feeaddb60dcdba4c091e30b939
Mahbub-Hasan2 <mahbubdev1@gmail.com>
2025-07-13 18:10:33 +0300

    navebar working

--- src/components/Navigations/Navbar.js
+++ src/components/Navigations/Navbar.js
@@ -1,126 +1,30 @@
 'use client';
 
-import { useState } from 'react';
 import MegaMenu from './MegaMenu';
-import Link from 'next/link';
+import SearchInput from './SearchInput';
+import HeaderIcons from './HeaderIcons';
 
 export default function Navbar() {
-    const [firstName, setFirstName] = useState(''); // Declare a state variable...
-
-    const [tab, setTab] = useState(false)
 
     return (
         <nav className="bg-white shadow">
             <div className="max-w-7xl mx-auto px-4 py-3 gap-x-10 flex items-center justify-between border-b border-gray-200">
                 <h1 className="text-xl font-bold">Qiftly</h1>
 
+                {/* Big search bar  */}
                 <div className="flex mr-auto w-1/2">
                     <div className="w-full max-w-xl mx-auto">
-                        <div className="relative flex items-center w-full border border-gray-300 rounded-xl overflow-hidden bg-white focus-within:ring-2 focus-within:ring-yellow-300 hover:ring-1 hover:ring-gray-300 transition">
-                            <input
-                                type="text"
-                                value={firstName}
-                                onChange={(e) => setFirstName(e.target.value)}
-                                placeholder="Search for something..."
-                                className="w-full px-4 py-2 outline-none"
-                            />
-
-                            {/* ক্লিয়ার বাটন */}
-                            {firstName && (
-                                <button
-                                    onClick={() => setFirstName("")}
-                                    className="text-gray-400 hover:text-black px-3"
-                                >
-                                    ×
-                                </button>
-                            )}
-
-                            {/* ডিভাইডার */}
-                            <div className="h-5 w-px bg-gray-200" />
-
-                            {/* সার্চ বাটন */}
-                            <button className="px-3 text-gray-600 hover:text-yellow-700 cursor-pointer">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="w-5 h-5"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                    strokeWidth={1.5}
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
+                        <SearchInput />
                     </div>
-
-
                 </div>
 
 
 
                 {/* icons  */}
-                <div className='flex justify-center gap-5'>
-                    <div className=''>
-                        <Link href="/account/user">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="transition-transform duration-300 ease-in-out hover:scale-110 cursor-pointer text-primary size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                            </svg>
-                        </Link>
-                    </div>
-                    <div className=''>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="transition-transform duration-300 ease-in-out hover:scale-110 cursor-pointer text-primary size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                        </svg>
-
-                    </div>
-                    <div className='relative '>
-                        <svg onClick={() => setTab(true)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="transition-transform duration-300 ease-in-out hover:scale-110 cursor-pointer text-primary size-6 opacity-80">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                        </svg>
-
-                        <div className={`z-10 ${tab ? "block" : "hidden"} absolute text-sm top-10 right-4 bg-white border border-gray-50 rounded-2xl p-5 shadow`}>
-                            <div className='relative'>
-
-                                <div className='cursor-pointer flex gap-3 text-gray-500 font-poppins mb-3'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="transition-transform duration-300 ease-in-out hover:scale-110 cursor-pointer text-primary size-5">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
-                                    </svg>
-
-                                    <span>FAQ</span>
-                                </div>
-                                <div className='cursor-pointer flex gap-3 text-gray-500 font-poppins mb-3'>
-
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="transition-transform duration-300 ease-in-out hover:scale-110 cursor-pointer text-primary size-5 opacity-80">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z" />
-                                    </svg>
-
-                                    <span>Collection</span>
-                                </div>
-                                <div className='cursor-pointer flex gap-3 text-gray-500 font-poppins'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="transition-transform duration-300 ease-in-out hover:scale-110 cursor-pointer text-primary size-5 opacity-80">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 3.75v4.5m0-4.5h-4.5m4.5 0-6 6m3 12c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
-                                    </svg>
-
-                                    <span className='whitespace-nowrap'>+974-3067-1369</span>
-                                </div>
-
-                                <div onClick={() => setTab(!tab)} className='hover:text-primary cursor-pointer absolute -top-14 left-6 w-full flex justify-end opacity-70 rotate-45'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                                    </svg>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
+                <HeaderIcons />
             </div>
+
+            {/* mega menu  */}
             <MegaMenu />
         </nav>
     );
