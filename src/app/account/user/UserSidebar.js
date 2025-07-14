fc654cdc9ce5a35e63ff407affd906f2523db47d
Ahsanul Haque <ahsanul0185@gmail.com>
2025-07-14 18:17:13 +0600

    sidebar design completed

--- src/app/account/user/UserSidebar.js
+++ src/app/account/user/UserSidebar.js
@@ -1,36 +1,54 @@
 "use client";
 
+import Image from "next/image";
 import { useEffect, useState } from "react";
+import {
+  User,
+  ShoppingBag,
+  MapPin,
+  CalendarHeart,
+  LogOut,
+  Trash,
+} from "lucide-react";
 
 const sidebarItems = [
   {
     hash: "profile",
     label: "Personal Information",
+    icon: User,
   },
   {
     hash: "orders",
     label: "My Orders",
+    icon: ShoppingBag,
   },
   {
     hash: "address",
     label: "Saved Addresses",
+    icon: MapPin,
   },
   {
     hash: "occassions",
     label: "My Occassions",
+    icon: CalendarHeart,
   },
   {
     hash: "",
     label: "Logout",
+    icon: LogOut,
   },
   {
     hash: "",
     label: "Delete Account",
+    icon: Trash,
   },
 ];
 
 export default function UserSidebar() {
   const [active, setActive] = useState("");
+  
+  const profileImg = null;
+  const fullName = "Munir Uddin Mahbub";
 
   useEffect(() => {
     const updateHash = () => setActive(window.location.hash.slice(1));
@@ -39,30 +57,45 @@ export default function UserSidebar() {
     return () => window.removeEventListener("hashchange", updateHash);
   }, []);
 
-
   const handleSidbarItemClick = (item) => {
-      window.location.hash =  item.hash;
-      setActive(item.hash)
-  }
+    window.location.hash = item.hash;
+    setActive(item.hash);
+  };
 
   return (
-    <div className="w-full md:w-1/4 border-r border-gray-200">
-      <div className="bg-olive-600 text-white rounded-t-xl py-4 px-4">
-        <h2 className="font-bold">Hey Munir Uddin Mahbub</h2>
+    <div className="w-full md:w-1/4 border-r border-gray-200  rounded-t-xl overflow-clip">
+      <div className="bg-primary text-white py-6 px-4 flex items-center gap-3">
+        {profileImg ? (
+          <Image src={profileImg} className="size-10 bg-amber-200 rounded-full border-2 object-cover border-secondary"/>
+        ) : (
+          <div className="size-10 shrink-0 bg-gray-200 text-gray-800 font-semibold grid place-items-center text-lg rounded-full border-2 border-secondary">
+            {fullName.split(" ")[0].slice(0, 1).toLocaleUpperCase() +
+              fullName.split(" ")[1]?.slice(0, 1).toLocaleUpperCase()}
+          </div>
+        )}
+        <h2 className="font-bold text-xl line-clamp-1">Hey {fullName}</h2>
       </div>
-      <ul className="bg-white">
+
+
+      <ul className="flex flex-col gap-4 mt-4">
         {sidebarItems.map((item) => (
           <li
-            key={item}
+            key={item.label}
             onClick={() => handleSidbarItemClick(item)}
-            className={`py-3 px-5 text-sm border-b hover:bg-gray-50 cursor-pointer ${
-              item.hash && active === item.hash ? "text-olive-700 font-semibold" : "text-gray-600"
+            className={` pr-5 text-md text-gray-400 hover:text-primary cursor-pointer flex items-center gap-6 group ${
+              item.hash && active === item.hash
+                ? "border-r-2 border-r-secondary text-primary"
+                : ""
             }`}
           >
+           <span className={`size-10 grid place-items-center rounded-full text-gray-400 group-hover:bg-gray-200 ${item.hash && item.hash === active ? "bg-gray-200" : ""}`}>
+            {<item.icon size={20} />}
+            </span> 
             {item.label}
           </li>
         ))}
       </ul>
+
     </div>
   );
 }
--- src/app/account/user/page.js
+++ src/app/account/user/page.js
@@ -5,7 +5,7 @@ import UserSidebar from "./UserSidebar";
 export default function Account() {
   return (
     <main className="bg-gray-50 min-h-screen">
-      <div className="max-w-6xl mx-auto py-10 px-4 flex flex-col md:flex-row gap-6">
+      <div className="max-w-7xl  mx-auto py-10 px-4 flex flex-col md:flex-row gap-6">
         <UserSidebar />
         <Contents />
       </div>
